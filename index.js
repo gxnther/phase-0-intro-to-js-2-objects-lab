0cb8e3facfa5a8cb136e8e10c8d5ec8ebb0f8964
Gunther <gunt.valentine@gmail.com>
2022-03-06 15:46:51 -0500

    push

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution in this file!
+const employee = {
+    name: "Gunther",
+    streetAddress: "2 Howard Dr., Greenbush, ME  04418",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeInfo = { ...employee } ;
+    
+    newEmployeeInfo[key] = value;
+
+    return newEmployeeInfo;
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "Sam",
+    "11 Broadway"
+);
+
+updateEmployeeWithKeyAndValue;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+const updatedAdress = destructivelyUpdateEmployeeWithKeyAndValue(employee, "Sam", "12 Broadway");
+
+destructivelyUpdateEmployeeWithKeyAndValue;
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employees = { ...employee };
+    employees[key] = employees.name;
+    delete employees.name;
+    return employees;
+}
+deleteFromEmployeeByKey;
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key] = employee.name;
+    delete employee.name
+    return employee;
+}
+
+destructivelyDeleteFromEmployeeByKey;
